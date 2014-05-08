b47617b17b91ce6c942658408b0b61cad67e1d03
viko16 <200522864@163.com>
2014-05-08 15:22:39 +0800

    1.0版本，完成替换播放器和判断内外网功能

---
+++ navodhtml5/content_scripts.js
@@ -0,0 +1,19 @@
+//判断是否播放页，非播放页无<object>的
+if (document.getElementsByTagName("object")[0] != undefined) {
+    //提取真实地址
+    var movieurl = $("object").find("param[name=FlashVars]").val().replace("&play_url=", "").replace("&play_url_hd=", "").split("&")[0];
+    //url编码解码
+    movieurl = unescape(movieurl);
+    //如果是外网访问，就替换一下路径
+    if ((window.location.href).indexOf("172.16") == -1) {
+        movieurl = movieurl.replace("172.16.31.102", "navod.scse.com.cn");
+    }
+    //移除旧播放器
+    $('object').remove();
+    //插入播放器
+    var v = $("<video id='hehevideo' class='video-js vjs-default-skin' controls preload='none' width='960' height='540' data-setup='{}'></video>");
+    $('.live_player').append(v);
+    //然后就是插真实地址咯
+    var r = ("<source src='" + movieurl + "' type='video/mp4' />");
+    $('#hehevideo').append(r);
+}
