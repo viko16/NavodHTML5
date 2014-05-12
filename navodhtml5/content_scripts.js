8be207ac8206d36356c35b978f3a7caf3ec32721
viko16 <200522864@163.com>
2014-05-12 18:54:19 +0800

    1.2版本，改变字体加载方式，完善播放记录功能

--- navodhtml5/content_scripts.js
+++ navodhtml5/content_scripts.js
@@ -5,9 +5,8 @@ if (document.getElementsByTagName("object")[0] != undefined) {
     //url编码解码
     movieurl = unescape(movieurl);
     //如果是外网访问，就替换一下路径
-    if ((window.location.href).indexOf("172.16") == -1) {
+    if ((window.location.href).indexOf("172.16") == -1)
         movieurl = movieurl.replace("172.16.31.102", "navod.scse.com.cn");
-    }
     //移除旧播放器
     $('object').remove();
     //插入播放器
@@ -16,11 +15,52 @@ if (document.getElementsByTagName("object")[0] != undefined) {
     //然后就是插真实地址咯
     var r = ("<source src='" + movieurl + "' type='video/mp4' />");
     $('#hehevideo').append(r);
+
+
     //增加音量记忆功能
     if (window.localStorage) {
         if (localStorage.volume) {
             var v = document.getElementsByTagName("video")[0];
-            v.volume = localStorage.volume;
+            v.volume = localStorage.volume; //设置音量
         }
     }
+
+
+    //插入一段css，调用字体图标
+    var c = document.createElement('style');
+    var font_css = "@font-face {font-family: 'VideoJS';";
+    font_css += "src: url('" + chrome.runtime.getURL("videojs/font/vjs.eot") + "');";
+    font_css += "src: url('" + chrome.runtime.getURL("videojs/font/vjs.eot") + "?#iefix') format('embedded-opentype'), ";
+    font_css += "url('" + chrome.runtime.getURL("videojs/font/vjs.woff") + "') format('woff'), ";
+    font_css += "url('" + chrome.runtime.getURL("videojs/font/vjs.ttf") + "') format('truetype');";
+    font_css += "font-weight: normal;font-style: normal;}";
+    c.innerHTML = font_css;
+    document.head.appendChild(c);
+
+
+
+    //淫荡的插入一段代码，注释中间才是正式代码
+    Function.prototype.getMultiLine = function() {
+        var lines = new String(this);
+        lines = lines.substring(lines.indexOf("/*") + 3, lines.lastIndexOf("*/"));
+        return lines;
+    }
+    var inject=function(){
+    /*
+        //在页面关闭时保存进度
+        window.onbeforeunload = dealHistoryDetail;
+        function dealHistoryDetail() {
+        var v = document.getElementsByTagName("video")[0];
+        if (!v.ended)  //影片没结束才把播放时长记下来，不然记录个最后没意思~
+            video_info.played_time = v.currentTime;
+        if(v.currentTime != 0) //没有播的就不用记录了
+            addHistory();
+    }
+    */}.getMultiLine();
+
+    var x=document.createElement("script");
+    x.type="text/javascript";
+    var y=document.createTextNode(inject);
+    x.appendChild(y);
+    document.body.appendChild(x);
 }
