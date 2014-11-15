d055819112775ce20003e431d223d9c36e729741
viko16 <16viko@gmail.com>
2014-11-16 00:32:39 +0800

    1.8版本，重构代码，优化显示

--- README.md
+++ README.md
@@ -7,20 +7,19 @@ Chrome插件：NA新在线影院 HTML5 播放器
 
 安装
 ==============
-1. 猛击我 [下载](https://github.com/viko16/navodhtml5/raw/master/navodhtml5.crx)
-1. 打开[「扩展程序」](chrome://extensions)页面
-1. 勾选「开发者模式」
-1. 将下载好的 `.crx` 文件拖到进去，猛戳一下「确定」！
-1. 搞掂！
+从 「Chrome网上应用店」 →[安装](https://chrome.google.com/webstore/detail/na%E5%9C%A8%E7%BA%BF%E5%BD%B1%E9%99%A2html5%E6%92%AD%E6%94%BE%E5%99%A8/eeonnpmppginengdnonfcmkmdfhaliam?hl=zh-CN)←
 
+_（由于上传到了应用商店，私钥不同会产生两只插件。以前下载安装包的童鞋可以先把旧的删掉，否则可能会产生冲突。）_
 
 作用
 ==============
 1. 用「HTML5 &lt;video&gt; 标签」替换了官方的「Flash 播放器」
-1. 方便「视频下载」（不信你右键看看！）
+1. 方便「视频下载」
 1. 增加了「音量记忆」功能（官方的没有提供）
 1. 完善了「播放列表」的记录功能
 1. 能使用快捷键调整进度、音量、全屏
+1. 优化了「选集框」位置
+1. 多集情况下直接播放最新一集
 
 
 快捷键
@@ -43,9 +42,14 @@ Chrome插件：NA新在线影院 HTML5 播放器
 ==============
 1. 此扩展只作用于 [`NA新版在线电影网`](http://navod.scse.com.cn) 的播放页面
 1. 这货是个 Chrome 扩展，请在 [Chrome 浏览器](http://www.google.cn/intl/zh-CN/chrome/) 上使用。未在各种双核浏览器中测试...
-1. 偷偷告诉你，`右键可以直接下载视频`~ ~
+1. 偷偷告诉你，`右键也可以下载视频`~ ~
 
 
 合作开发者
 ==============
-感谢 @xingrz 和 @ONEWateR
+感谢（先后顺序仅为提交 Pull requests 时间）
+
+- @ONEWateR
+- @xingrz
+- @karonl
+- @djyde
--- navodhtml5/content_scripts.js
+++ navodhtml5/content_scripts.js
@@ -1,23 +1,10 @@
-//insertCss('newcss.css');
 var flash = $('embed#player_fg[FlashVars]');
-//读取值对信息，判断是否播放最新一集
-function GetRequest() {
-    var url = location.search; //获取url中"?"符后的字串
-    var theRequest = new Object();
-    if (url.indexOf("?") != -1) {
-        var str = url.substr(1);
-        strs = str.split("&");
-        for (var i = 0; i < strs.length; i++) {
-            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-        }
-    }
-    return theRequest;
-}
 
 if (flash.length > 0) {
+
+    // 自动播放最新一集
     var Request = new Object();
     Request = GetRequest();
-    var namelast = "";
     if (Request['nns_video_index'] > -1) {
         //手动选择的集数
     } else {
@@ -33,57 +20,48 @@ if (flash.length > 0) {
     var movieurl = flash.attr('FlashVars').match(/play_url\=([^\&]+)/)[1];
 
     // 替换路径为域名，方便内外网都能访问
-    movieurl = unescape(movieurl)
+    movieurl = decodeURIComponent(movieurl)
         .replace("172.16.31.101", "navod.scse.com.cn")
         .replace("172.16.31.102", "navod.scse.com.cn")
         .replace("172.16.31.103", "navod.scse.com.cn");
 
+    // 配置 video 标签
     var player = $('<video>');
     player.css('width', '100%');
-    player.attr('src', movieurl);
-    player.attr('autoplay', true);
-    player.attr('controls', true);
-    player.attr('preload', 'auto');
+    player.attr({
+        src: movieurl,
+        autoplay: true,
+        controls: true,
+        preload: 'auto'
+    });
 
-    var ss = $('.player_msgs_right');
-    var ch = $('.same_movie');
-    var download = $('<div>');
-    var theater = $('<div>');
-
-    var wrapper = $('<div id="zx">');
+    // 嘘，静悄悄的把播放器换掉，不要告诉其他人
+    var wrapper = $('<div>');
     wrapper.css('width', 960);
     wrapper.css('margin', '0 auto');
-    //增加侧边栏，方便下载和其他功能
-    download.html('<div class="same_movie" style="overflow: hidden;"><div class="same_movie_title">下载视频</div><a style="float:left;background: #70A3F7;color: #fff;width: 100px;border-radius: 3px;margin: 10px;padding: 10px;text-align: center;" href="' + movieurl + '">点击下载</a><a style="float:left;background: #FF3F1E;color: #fff;width: 100px;border-radius: 3px;margin: 10px;padding: 10px;text-align: center;" href="javascript:void(0);" id="showthe" >剧场模式</a></div>');
-    ss.empty().append(download);
-    ss.append(ch);
-
-    var nameaddress = $('.daohang_content_nowaddress');
-    var movename = nameaddress.html().match(/<b.*?>(.*?)<\/b>/)[1].split("（");
-    //电影名字突出显示
-    nameaddress.css({'display': 'none'});
-
-    var titlediv = $('<div>');
-    titlediv.css({
-        'font-family': '微软雅黑',
-        'width': '960px',
-        'overflow': 'hidden',
-        'margin': '0 auto',
-        'font-size': '20px',
-        'height': '40px'
-    });
-    titlediv.html(movename[0] + " " + namelast);
-
     wrapper.append(player);
-    $('.live_content').empty();
-    $('.live_content').append(titlediv);
-    $('.live_content').css({'height': 'auto', 'background': '#FEFAF7'}).append(wrapper);
+    $('.live_content').css('height', 'auto').empty().append(wrapper);
 
-    // 音量记忆
-    if (!isNaN(parseFloat(localStorage.volume))) {
-        player.prop('volume', localStorage.volume);
-    }
 
+    // 添加“辅助功能区”，方便下载和其他功能
+    var helperblock = $('<div class="same_movie">').css('overflow', 'hidden');
+    var helpertitle = $('<div class="same_movie_title">').text('辅助功能');
+    var downloadbutton = $('<a>').attr('href', movieurl).text('点击下载');
+    downloadbutton.css({
+        'background': '#70A3F7',
+        'color': '#fff',
+        'width': '100px',
+        'border-radius': '3px',
+        'margin': '10px auto',
+        'padding': '10px',
+        'text-align': 'center'
+    });
+    helperblock.append(helpertitle, downloadbutton); //此处可放更多功能按钮
+    //放进侧边栏
+    $('.player_msgs_right').prepend(helperblock);
+
+
+    // 单击暂停|播放
     player.on('click', function () {
         if (this.paused) {
             this.play();
@@ -91,17 +69,9 @@ if (flash.length > 0) {
             this.pause();
         }
     });
-    //版本 37.0.2062.102 m 拖动BUG 修复
-    player.on('volumechange', function (volume) {
-        localStorage.volume = player.prop('volume');
-        this.play();
-    });
 
-    //版本 37.0.2062.102 m 拖动BUG 修复
-    player.on('seeked', function () {
-        this.play();
-    });
 
+    // 双击全屏|退出
     player.on('dblclick', function () {
         if (document.webkitIsFullScreen) {
             document.webkitExitFullscreen();
@@ -109,6 +79,23 @@ if (flash.length > 0) {
             this.webkitRequestFullScreen();
         }
     });
+
+
+    // 音量记忆
+    if (!isNaN(parseFloat(localStorage.volume))) {
+        player.prop('volume', localStorage.volume);
+    }
+    player.on('volumechange', function (volume) {
+        localStorage.volume = player.prop('volume');
+        this.play();
+    });
+
+
+    // 版本 37.0.2062.102 m 拖动BUG 修复
+    player.on('seeked', function () {
+        this.play();
+    });
+
 
     // 快捷键
     $(document.body).on('keydown', function (evt) {
@@ -148,74 +135,61 @@ if (flash.length > 0) {
     });
 
 
-    // 往原页面插 script
-    function insertScript(path) {
-        var script = document.createElement('script');
-        script.src = chrome.extension.getURL(path);
-        document.body.appendChild(script);
-    }
-
-
     // 播放位置记忆功能
     insertScript('extra.js');
-    //剧场模式
-    var state = 0;
-    $("#showthe").click(function () {
-        if (state == 0) {
-            $("#zx").animate({'width': '100%', 'left': '0px', 'top': '20%'});
-            $('body').animate({scrollTop: $("#zx").position().top + "px"}, 100);
-            state = 1;
-        } else {
-            $("#zx").animate({'width': '960px', 'margin': '0 auto'});
-            $('body').animate({scrollTop: $(".top").position().top + "px"}, 100);
-            state = 0;
-        }
-    });
 
 
-}
-function showselect() {
+    // 优化剧集列表显示位置
     if ($('.movie_page_index').length > 0) {
         $("#title_btn_two").css({'display': 'none'});
-        var temp = $(".movie_remarks").html();
-        var selects = $('<div>');
+        var selects = $("<div style='width:100%;overflow: hidden;margin-top: 20px;'>");
         $('.movie_page_index').each(function () {
             selects.append($(this));
         });
-        $(".movie_remarks").empty();
-        $(".movie_remarks").html("<div style='width:100%;overflow: hidden;margin-top: 20px;'>" + selects.html() + "</div>" + "<div id='movie_remarks'>" + temp + "</div>");
-        $(".movie_remarks").attr('class', '');
-        $("#movie_remarks").attr('class', 'movie_remarks');
+        $('.movie_remarks').before(selects);
     }
 
+} // end main
+
+
+/**
+ * 读取值对信息，判断是否播放最新一集
+ * @returns {Object}
+ * @constructor
+ */
+function GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
 }
 
-//读取新css
+/**
+ * 往原页面插 JavaScript
+ * @param path 文件路径
+ */
+function insertScript(path) {
+    var script = document.createElement('script');
+    script.src = chrome.extension.getURL(path);
+    document.body.appendChild(script);
+}
+
+/**
+ * 往原页面插 CSS
+ * @param path 文件路径
+ */
 function insertCss(path) {
     var node = document.createElement("link");
     node.setAttribute("rel", "stylesheet");
     node.setAttribute("type", "text/css");
     node.setAttribute("href", chrome.extension.getURL(path));
     document.body.appendChild(node);
-    $('img').each(function () {
-        if ($(this).attr('src') == 'images/look_btn.png') {
-            var sa = $('<div>');
-            sa.html("点击播放");
-            sa.css({
-                'width': '70px',
-                'background': '#3681D5',
-                'border-radius': '3px',
-                'padding': '6px 1px 6px 13px',
-                'font-size': '14px',
-                'color': '#fff'
-            });
-            $(this).parent().append(sa);
-            $(this).attr('src', '');
-
-        }
-    });
-
 }
-var selectbutton = $('.select_btn');
-selectbutton.attr('value', '搜索');
-showselect();
+
+
